6868078e71fadfd76cc27b70e2312edf8d4557ad
promitey864-zoompull <promitey864@gmail.com>
2026-09-25 23:11:47 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,19 @@
+import 'package:flutter/material.dart';
+import 'game_board.dart';
 
+void main() {
+  runApp(const ChessCheckersApp());
+}
+
+class ChessCheckersApp extends StatelessWidget {
+  const ChessCheckersApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Шахматы против Шашек',
+      theme: ThemeData.dark(),
+      home: const GameBoardSquare(),
+    );
+  }
+}
